2144155a79928101109447ce96b3b23328a3a7b2
yin-qian <yinqian_qian@163.com>
2017-07-18 17:54:12 +0800

    Site updated: 2017-07-18 17:54:10

--- pic/photo.js
+++ pic/photo.js
@@ -2,7 +2,7 @@ var render = function(page, data){
   var img = "";
   for (var i = 0; i < data.length; i++) {
     img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://osixq3h44.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://osixq3h44.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
-    // img += '<img src="http://osixq3h44.bkt.clouddn.com/' + data[i] + '">';
+    img += '<img src="http://osixq3h44.bkt.clouddn.com/' + data[i] + '">';
   }
   $(".img-box-ul").append(img);
   $(".img-box-ul").lazyload();
