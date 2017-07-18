8c0b43a9025b841d7953cbd9c751372b12bc710c
yin-qian <yinqian_qian@163.com>
2017-07-18 16:38:29 +0800

    Site updated: 2017-07-18 16:38:27

--- pic/photo.js
+++ pic/photo.js
@@ -1,7 +1,7 @@
 var render = function(page, data){
   var img = "";
   for (var i = 0; i < data.length; i++) {
-    img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://yourqiniu.url.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://yourqiniu.url.com/' + data[i] + '">'  + '</div></li>';
+    img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://osixq3h44.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://osixq3h44.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
     //img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
   }
   $(".img-box-ul").append(img);
