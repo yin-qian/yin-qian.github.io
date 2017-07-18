74c62baa2c238e13912595a13ce90b678767fc6c
yin-qian <yinqian_qian@163.com>
2017-07-18 13:05:57 +0800

    Site updated: 2017-07-18 13:05:57

---
+++ photo.js
@@ -0,0 +1,10 @@
+var render = function(page, data){
+	var img = "";
+	for (var i = 0; i < data.length; i++) {
+		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://yourqiniu.url.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://yourqiniu.url.com/' + data[i] + '">'  + '</div></li>';
+		//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
+	}
+	$(".img-box-ul").append(img);
+	$(".img-box-ul").lazyload();
+	$("a[rel=example_group]").fancybox();
+}
